81c52d0a485dd5e775cec87ba952b88b41ab41dc
matthewxrao <s412272@student.roundrockisd.org>
2024-05-01 16:20:40 -0500

    FixedErrors

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -14,18 +14,12 @@ namespace Elephonkey
 
             if(getuserSavedkey == true)
             {
-                MyAppShell.CurrentItem = HomeTab;
+                MyAppShell.CurrentItem = MyHomePage;
             }
             else
             {
                 MyAppShell.CurrentItem = MyLoginPage;
             }
-
-            this.HomeTab.Icon = ImageSource.FromResource("Elephonkey.Resources.Images.home.svg", this.GetType().Assembly);
-            this.SurveyTab.Icon = ImageSource.FromResource("Elephonkey.Resources.Images.survey.png", this.GetType().Assembly);
-            this.ArticlesTab.Icon = ImageSource.FromResource("Elephonkey.Resources.Images.articles.svg", this.GetType().Assembly);
-            this.ResultsTab.Icon = ImageSource.FromResource("Elephonkey.Resources.Images.results.svg", this.GetType().Assembly);
-            this.SettingsTab.Icon = ImageSource.FromResource("Elephonkey.Resources.Images.settings.svg", this.GetType().Assembly);
         }
     }
 }
